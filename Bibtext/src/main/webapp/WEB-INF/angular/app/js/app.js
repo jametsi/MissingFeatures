598f2fd16ce2e3e5c97c9a86a3dbfe4d2b6f4d08
Jami Karvanen <jami@karvanen.fi>
2013-04-12 17:12:38 +0300

    Bootstrap-menu & submit

--- Bibtext/src/main/webapp/WEB-INF/angular/app/js/app.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/list', {templateUrl: 'partials/listreferences.html', controller: listController});
     $routeProvider.when('/submit', {templateUrl: 'partials/submitreference.html', controller: submissionController});
