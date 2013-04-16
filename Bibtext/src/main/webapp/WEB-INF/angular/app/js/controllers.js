a2f5081e88521982e8ea8db914a57fa4ba45e63e
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-04-16 23:48:21 +0300

    cleaning up

--- Bibtext/src/main/webapp/WEB-INF/angular/app/js/controllers.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/app/js/controllers.js
@@ -3,30 +3,27 @@
 /* Controllers */
 
 function listController($scope, $http) {
-    
+
     // Getter method for JSON data from backend
     $scope.getListing = function() {
-            $http.get('../rest').success(function(data) {
-        $scope.references = data;
-    });
-    }
+        $http.get('../rest').success(function(data) {
+            $scope.references = data;
+        });
+    };
     // Default action after page load
     $scope.getListing();
-};
+}
+;
 
 function referenceDetailController($scope, $http) {
-    
+
     //tobedone
-};
+}
+;
 
 function submissionController($scope, $http, $location) {
- 
-    // Clear the book at first
-    // Update the object whenever a letter is typed
-  //  $scope.update = function(book) {
- //       $scope.master = angular.copy(book);
- //   };
-    
+
+    // New author object
     $scope.newAuthorField = function() {
         $scope.reference.authors.push({"name": ""});
     };
@@ -34,26 +31,32 @@ function submissionController($scope, $http, $location) {
     // Empty the book data
     $scope.reset = function() {
         $scope.reference = {
-            "authors" : [{"name": ""}]
+            "authors": [{"name": ""}]
         };
     };
-    
+
     function authorsToStringArray(reference) {
         var resultArray = [];
-        for(var i = 0 ; i < reference.authors.length ; ++i) {
+        for (var i = 0; i < reference.authors.length; ++i) {
             resultArray.push(reference.authors[i].name);
-        };
+        }
+        ;
         return resultArray;
-    };
- 
+    }
+    ;
+
     // Submit the HTTP Post to backend REST URL
     $scope.submit = function(reference) {
         reference["authors"] = authorsToStringArray(reference);
         $http.post('../rest', angular.toJson(reference), {
             'Content-Type': 'application/json'
-        }).success(function(date) { $location.path("/list")});
+        }).success(function(date) {
+            $location.path("/list")
+        });
         $scope.reset();
     };
+    // Default actions to run on start
     $scope.reset();
-    
-};
+
+}
+;
