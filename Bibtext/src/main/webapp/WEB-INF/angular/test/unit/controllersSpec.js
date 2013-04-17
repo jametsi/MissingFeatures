9ab9b3e574d3d625e23850f66fd4a7688f65df10
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-04-17 13:01:20 +0300

    tests for listcontroller

--- Bibtext/src/main/webapp/WEB-INF/angular/test/unit/controllersSpec.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/test/unit/controllersSpec.js
@@ -2,30 +2,43 @@
 
 /* jasmine specs for controllers go here */
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
+describe('listController', function() {
 
-  beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
-  });
+    var $httpBackend, $controller, scope;
 
+    beforeEach(inject(function($injector, $rootScope, $controller) {
 
-  it('should ....', function() {
-    //spec body
-  });
-});
+        $httpBackend = $injector.get('$httpBackend');
+        $httpBackend.when('GET', '../rest').respond([
+            {"id": 1, "authors": ["421"], "title": "421", "year": 421, "bibtextID": null, "type": null, "booktitle": null, "publisher": "421", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 2, "authors": ["safdsafd"], "title": "fdasfd", "year": 4324, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fdsafds", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 3, "authors": ["fds"], "title": "fds", "year": 4334, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fdsfas", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 4, "authors": ["fsa"], "title": "sa", "year": 4242, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fasfsa", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 5, "authors": ["fasfs"], "title": "fassa", "year": 6666, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fasfsa", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0}]);
+        scope = $rootScope.$new();
 
+    }));
 
-describe('MyCtrl2', function(){
-  var myCtrl2;
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
 
+    it('should display a proper amount of elements', inject(function($controller) {
+        var controller = $controller('listController', {
+            $scope: scope
+        });
+        $httpBackend.flush();
+        expect(scope.references.length).toBe(5);
 
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
-  });
+    }));
 
+    it('should fetch data on load', inject(function($controller) {
+        $httpBackend.expectGET('../rest');
+        var controller = $controller('listController', {
+            $scope: scope
+        });
+        $httpBackend.flush();
+    }));
 
-  it('should ....', function() {
-    //spec body
-  });
 });
