1b7238edff0296c87080cb18c858ecd76c3d7e79
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-04-17 13:12:50 +0300

    One more test to cover the fucking shit

--- Bibtext/src/main/webapp/WEB-INF/angular/test/unit/controllersSpec.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/test/unit/controllersSpec.js
@@ -7,14 +7,8 @@ describe('listController', function() {
     var $httpBackend, $controller, scope;
 
     beforeEach(inject(function($injector, $rootScope, $controller) {
-
         $httpBackend = $injector.get('$httpBackend');
-        $httpBackend.when('GET', '../rest').respond([
-            {"id": 1, "authors": ["421"], "title": "421", "year": 421, "bibtextID": null, "type": null, "booktitle": null, "publisher": "421", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
-            {"id": 2, "authors": ["safdsafd"], "title": "fdasfd", "year": 4324, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fdsafds", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
-            {"id": 3, "authors": ["fds"], "title": "fds", "year": 4334, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fdsfas", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
-            {"id": 4, "authors": ["fsa"], "title": "sa", "year": 4242, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fasfsa", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
-            {"id": 5, "authors": ["fasfs"], "title": "fassa", "year": 6666, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fasfsa", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0}]);
+
         scope = $rootScope.$new();
 
     }));
@@ -25,6 +19,13 @@ describe('listController', function() {
     });
 
     it('should display a proper amount of elements', inject(function($controller) {
+        $httpBackend.when('GET', '../rest').respond([
+            {"id": 1, "authors": ["421"], "title": "421", "year": 421, "bibtextID": null, "type": null, "booktitle": null, "publisher": "421", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 2, "authors": ["safdsafd"], "title": "fdasfd", "year": 4324, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fdsafds", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 3, "authors": ["fds"], "title": "fds", "year": 4334, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fdsfas", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 4, "authors": ["fsa"], "title": "sa", "year": 4242, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fasfsa", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0},
+            {"id": 5, "authors": ["fasfs"], "title": "fassa", "year": 6666, "bibtextID": null, "type": null, "booktitle": null, "publisher": "fasfsa", "pages": null, "address": null, "journal": null, "volume": 0, "number": 0}]);
+        
         var controller = $controller('listController', {
             $scope: scope
         });
@@ -34,11 +35,23 @@ describe('listController', function() {
     }));
 
     it('should fetch data on load', inject(function($controller) {
+        $httpBackend.when('GET', '../rest').respond([]);
         $httpBackend.expectGET('../rest');
         var controller = $controller('listController', {
             $scope: scope
         });
         $httpBackend.flush();
+    }));
+
+    it('should give no answers', inject(function($controller) {
+        $httpBackend.when('GET', '../rest').respond([]);
+        $httpBackend.expectGET('../rest');
+        
+        var controller = $controller('listController', {
+            $scope: scope
+        });
+        $httpBackend.flush();
+        expect(scope.references.length).toBe(0);
     }));
 
 });
