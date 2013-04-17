947f620e3f2e8afd3da512d8bbbb34d6cc66170c
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-04-17 13:36:25 +0300

    renamed myApp -> frontSideAngular

--- Bibtext/src/main/webapp/WEB-INF/angular/app/js/app.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.bootstrap']).
+angular.module('frontSideAngular', ['frontSideAngular.filters', 'frontSideAngular.services', 'frontSideAngular.directives', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/list', {templateUrl: 'partials/listreferences.html', controller: listController}).
                     when('/submit', {templateUrl: 'partials/submitreference.html', controller: submissionController}).
