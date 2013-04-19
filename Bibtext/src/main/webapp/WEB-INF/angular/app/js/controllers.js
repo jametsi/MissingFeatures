efd80dbabe1ce159bc1850a769f5a172ee16e110
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-04-19 11:21:59 +0300

    bibtextID generation and shit

--- Bibtext/src/main/webapp/WEB-INF/angular/app/js/controllers.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/app/js/controllers.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 function listController($scope, $http) {
-    
+
     $scope.references = [];
 
     // Getter method for JSON data from backend
@@ -14,15 +14,17 @@ function listController($scope, $http) {
     };
     // Default action after page load
     this.getListing();
-};
+}
+;
 
 function referenceDetailController($scope, $http, $routeParams) {
-    $http.get('../rest/'+$routeParams.referenceId).success(function(data) {
-       $scope.reference = data;
+    $http.get('../rest/' + $routeParams.referenceId).success(function(data) {
+        $scope.reference = data;
         console.log($scope.reference);
     });
 
-};
+}
+;
 
 function submissionController($scope, $http, $location) {
 
@@ -47,9 +49,30 @@ function submissionController($scope, $http, $location) {
         return resultArray;
     };
 
+    $scope.generateBibtextID = function(reference) {
+        if ((!reference ||
+                reference.year.length < 4) ||
+                reference.authors[0].name.charAt(' ') === -1) {
+            return;
+        }
+        var year = reference.year.substring(2, 4);
+        var result = "";
+        for (var i = 0; i < reference.authors.length; ++i) {
+            var name = reference.authors[i].name;
+            if (name.indexOf(',') !== -1) {
+                result += name.split(", ")[0].charAt(0);
+            } else {
+                result += name.split(" ")[1].charAt(0);
+            }
+        }
+        return result + year;
+    };
+
     // Submit the HTTP Post to backend REST URL
     $scope.submit = function(reference) {
+        reference.bibtextID = this.generateBibtextID(reference);
         reference["authors"] = this.authorsToStringArray(reference);
+
         $http.post('../rest', angular.toJson(reference), {
             'Content-Type': 'application/json'
         }).success(function(date) {
@@ -60,4 +83,5 @@ function submissionController($scope, $http, $location) {
     // Default actions to run on start
     $scope.reset();
 
-};
+}
+;
