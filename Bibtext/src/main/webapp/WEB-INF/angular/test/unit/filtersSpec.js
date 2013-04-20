e0982e568db4e98b1bf3c308b214e5627b5046f0
Jami Karvanen <jametsi@localhost.localdomain>
2013-04-20 21:10:08 +0300

    fixed concat-filter test

--- Bibtext/src/main/webapp/WEB-INF/angular/test/unit/filtersSpec.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/test/unit/filtersSpec.js
@@ -7,12 +7,12 @@ describe('filter', function() {
 
   describe('concat', function() {
     beforeEach(module(function($provide) {
-      $provide.value('authors', ["Jami Karvanen", "Ville Heikkinen"]);
+//      $provide.value(["Jami Karvanen", "Ville Heikkinen"]);
     }));
 
 
-    it('should replace AUTHORS', inject(function(concatFilter) {
-      expect(concatFilter('before %AUTHORS% after')).toEqual('before Jami Karvanen, Ville Heikkinen after');
+    it('should replace author-array with concatenated string', inject(function(concatFilter) {
+      expect(concatFilter(["Jami Karvanen", "Ville Heikkinen"])).toEqual('Jami Karvanen, Ville Heikkinen');
     }));
   });
 });
