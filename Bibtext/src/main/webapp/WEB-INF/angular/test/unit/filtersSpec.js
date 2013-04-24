76c48af22d02fa95e3dee2766b6c67450fe12d8c
Ville Heikkinen <ville.heikkinen@gmail.com>
2013-04-24 23:02:40 +0300

    added test for scandinavian chars

--- Bibtext/src/main/webapp/WEB-INF/angular/test/unit/filtersSpec.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/test/unit/filtersSpec.js
@@ -15,4 +15,32 @@ describe('filter', function() {
       expect(concatFilter(["Jami Karvanen", "Ville Heikkinen"])).toEqual('Jami Karvanen, Ville Heikkinen');
     }));
   });
+  
+    describe('bibtext', function() {
+        
+    beforeEach(module(function($provide) {
+    }));
+
+
+    it('should replace scandinavian chars with bibtex symbols', inject(function(bibtextFilter) {
+        var testObject = {
+            "id":1,
+            "authors":["Dave Gahan"],
+            "title":"Känni-ääliÖiden",
+            "year":2012,
+            "bibtextID":"G12",
+            "type":"Book",
+            "booktitle":null,
+            "publisher": "öÖäÄ",
+            "pages":null,"address":null,
+            "journal":null,
+            "volume":0,
+            "number":0
+        };
+        var testObject = bibtextFilter(testObject);
+      expect(testObject.title).toEqual('K\\\"{a}nni-\\\"{a}\\\"{a}li\\\"{O}iden');
+      expect(testObject.publisher).toEqual('\\\"{o}\\\"{O}\\\"{a}\\\"{A}')
+    }));
+    
+  });
 });
