5b85201e182e433270b0f6a8836bc38e6400d56c
Ville Heikkinen <ville.heikkinen@cs.helsinki.fi>
2013-04-25 15:06:22 +0300

    fixed file extension

--- Bibtext/src/main/webapp/WEB-INF/angular/app/js/controllers.js
+++ Bibtext/src/main/webapp/WEB-INF/angular/app/js/controllers.js
@@ -12,17 +12,16 @@ function listController($scope, $http) {
             $scope.references = data;
         });
     };
-
-    
+   
     
     $scope.save = function() {
         var bb = new BlobBuilder();
         bb.append(angular.element('.well').text())
         var blob = bb.getBlob("text/plain;charset=" + document.characterSet);
         var filename = $scope.file ? $scope.file.filename+".bib" : "default.bib";
-         saveAs(blob, filename);
+        saveAs(blob, filename);
     };
-        // Default action after page load
+    // Default action after page load
     $scope.getListing();
     
     $scope.deleteReference = function(id) {
@@ -37,8 +36,7 @@ function referenceDetailController($scope, $http, $routeParams) {
         $scope.reference = data;
     });
 
-}
-;
+};
 
 function submissionController($scope, $http, $location, $routeParams) {
     
@@ -49,7 +47,9 @@ function submissionController($scope, $http, $location, $routeParams) {
             var authors = [];
             console.log($scope.reference.authors);
             for(var i = 0; i < $scope.reference.authors.length;i++) {
-                authors.push({name:$scope.reference.authors[i]});
+                authors.push({
+                    name:$scope.reference.authors[i]
+                });
             }
             
             $scope.reference.authors = authors;
@@ -66,13 +66,17 @@ function submissionController($scope, $http, $location, $routeParams) {
     }
     // New author object
     $scope.newAuthorField = function() {
-        $scope.reference.authors.push({"name": ""});
+        $scope.reference.authors.push({
+            "name": ""
+        });
     };
 
     // Empty the book data
     $scope.reset = function() {
         $scope.reference = {
-            "authors": [{"name": ""}],
+            "authors": [{
+                "name": ""
+            }],
             "type": "Book"
         };
     };
@@ -94,8 +98,8 @@ function submissionController($scope, $http, $location, $routeParams) {
     
     $scope.generateBibtextID = function(reference) {
         if ((!reference ||
-                reference.year.length < 4) ||
-                reference.authors[0].name.charAt(' ') === -1) {
+            reference.year.length < 4) ||
+        reference.authors[0].name.charAt(' ') === -1) {
             return "";
         }
         reference.year += "";
@@ -119,7 +123,7 @@ function submissionController($scope, $http, $location, $routeParams) {
 
         if ($scope.modify) {
             $http.post('../rest/'+$scope.reference.id, angular.toJson(reference), {
-            'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             }).success(function(date) {
                 $location.path("/list")
             });
@@ -139,5 +143,54 @@ function submissionController($scope, $http, $location, $routeParams) {
             $scope.reference = data;
         });
     };
+    
+    $scope.debugSubmit = function() {
+        var obj1 = {
+            type: "inproceedings",
+            authors: [
+            {
+                name: "Arto Vihavainen"
+            },
+
+            {
+                name: "Matti Paksula"
+            },
+
+            {
+                name: "Matti Luukkainen"
+            }],
+            title: "Extreme Apprenticeship Method in Teaching Programming for Beginners.",
+            year: 2011,
+            booktitle: "SIGCSE '11: Proceedings of the 42nd SIGCSE technical symposium on Computer science education"
+        }
+        var obj2 = {
+            type: "book",
+            authors: [{
+                name: "Robert Martin"
+            }],
+            title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+            year: 2008,
+            publisher: "Prentice Hall"
+        }
+        var obj3 = {
+            type: "article",
+            authors: [{
+                name: "Keith J. Whittington"
+            }],
+            title: "Infusing active learning into introductory programming courses",
+            journal : "J. Comput. Small Coll.",
+            volume : 19,
+            number : 5,
+            year : 2004,
+            pages : "249-259",
+            publisher : "Consortium for Computing Sciences in Colleges",
+            address : "USA"
+        }
+    
+        this.submit(obj1);
+        this.submit(obj2);
+        this.submit(obj3);
+       
+    }
 
 };
